8895fe157e0adf5ce0ec8eedcb528177ab7c5d98
Mathew Bramson <mathewbramson@gmail.com>
2018-06-05 13:33:35 -0400

    Add prolog hello-world

---
+++ prolog/hello-world/hello_world.pl
@@ -0,0 +1,11 @@
+% Please visit http://exercism.io/languages/prolog/installation
+% for instructions on setting up prolog.
+% Visit http://exercism.io/languages/prolog/tests
+% for help running the tests for prolog exercises.
+
+% Replace the goal below with
+% your implementation.
+
+hello_world(String) :- format(String).
+
+hello_world(Name, String) :- format(String).
